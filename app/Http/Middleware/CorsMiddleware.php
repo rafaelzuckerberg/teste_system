9607ade45d11f5c8dfe57fddc27fc7129cc90bcf
rafaelzuckerberg <contatorafaelmoura@gmail.com>
2019-08-23 01:32:14 -0300

    Update Middleware Cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,7 +16,7 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         if($request->is('api/*') && \App::environment('local')) {
-            header('Access-Control-Allow-Origin: http://127.0.0.1:8000');
+            header('Access-Control-Allow-Origin: https://testesystem.herokuapp.com');
             header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE');
             header('Access-Control-Allow-Headers: Content-Type');
           }
