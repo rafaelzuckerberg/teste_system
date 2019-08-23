139c5a605b545e19279bb7ea44b071c025a64b76
rafaelzuckerberg <contatorafaelmoura@gmail.com>
2019-08-23 02:23:46 -0300

    Update Middleware Cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,14 +16,9 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         if($request->is('api/*') && \App::environment('local')) {
-            // header('Access-Control-Allow-Origin: https://testesystem.herokuapp.com');
-            // header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE');
-            // header('Access-Control-Allow-Headers: Content-Type');
-            header('Access-Control-Allow-Origin: *'); 
-            header("Access-Control-Allow-Credentials: true");
-            header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-            header('Access-Control-Max-Age: 1000');
-            header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+            header('Access-Control-Allow-Origin: https://testesystem.herokuapp.com');
+            header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE');
+            header('Access-Control-Allow-Headers: Content-Type');
           }
           return $next($request);
     }
